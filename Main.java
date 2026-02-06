a02ff7c23f88736aa932f0cf6ccaec07c9ec705a
varshadevi18 <itzvarshadevi@gmail.com>
2026-02-06 10:46:50 +0530

    Methods in java

---
+++ Main.java
@@ -0,0 +1,35 @@
+class Computer{
+    // public void add(){
+    //     System.out.println( "Adding..");
+    // }
+    // public double add1(double a,int b,int c){
+    //     return a+b+c;
+    // }
+    // inorder to accept diff parameters..we are doing method overloading 
+    //samefunc with diff parameter
+    
+    public int add(int n1,int n2,int n3){
+        return n1+n2+n3;
+    }
+    public int add(int n1,int n2){
+        return n1+n2;
+    }
+    //we have also define for diff datatype
+    public double add(int a, double b){
+        return a+b;
+    }
+    //double+int= double(so return type shld be double)
+
+
+}
+class Main {
+    public static void main(String[] args) {
+        Computer obj= new Computer();
+    int r= obj.add(5,7,8);
+    int r1=obj.add(5,7);
+    System.out.println(r);
+    System.out.println(r1);
+    double ans= obj.add(5, 2.5);
+    System.out.println(ans);
+    }
+}
