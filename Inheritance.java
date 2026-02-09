3747fe8173b74c7abaf7f6d7b028c14e55d5ea1d
varshadevi18 <itzvarshadevi@gmail.com>
2026-02-09 20:13:41 +0530

    Types of Inheritance

--- Inheritance.java
+++ Inheritance.java
@@ -14,19 +14,26 @@ class Advcalc extends Calc{
     // child class
     //with this object we can access method from this and parent class
 }
+class Allone extends Advcalc{
+    public int div(int a, int b){
+        return a/b;
+    }
+}
 public class Inheritance {
     public static void main(String[] args){
         // Calc obj= new Calc();
         // int sum=obj.add(5,6);
         // int sub=obj.sub(5, 3);
-        Advcalc obj1= new Advcalc();
+        // Advcalc obj1= new Advcalc(); 
+        Allone obj1= new Allone(); // multilevel inheritance
         int sum= obj1.add(5,6);
         int sub=obj1.sub(6,3);
         int p= obj1.power(4, 2);
+        int div= obj1.div(10, 5);
         System.out.println(sum);
         System.out.println(sub);
         System.out.println(p);
-
+        System.out.println(div);
     }  
 
 }
