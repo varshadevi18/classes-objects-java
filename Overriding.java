0b4ec1c8693d2d6f5d862ae46d306ff1742a05e4
varshadevi18 <itzvarshadevi@gmail.com>
2026-02-09 20:37:15 +0530

    Method overriding in java

--- Overriding.java
+++ Overriding.java
@@ -1,21 +1,30 @@
 class A{
-    public void show(){
-        System.out.println("in A");
+    // public void show(){
+    //     System.out.println("in A");
+    // }
+    public int add(int a,int b){
+        return a+b;
     }
 }
 class B extends A{
     //it has no methods so calls from parent
     //now we do overriding to print B with same show()
-    public void show(){
-        System.out.println("in B");
-    }
+    // public void show(){
+    //     System.out.println("in B");
+    // }
     //this gets printed and it overides method of A class
+    public int add(int x,int y){
+        return x+y+1;
+    }
+    //overrided add method from parent class
 
 }
 public class Overriding{
     public static void main(String[] args) {
         B obj= new B();
-        obj.show();  
+        int ans=obj.add(5,5);
+        System.out.println(ans);
+
     }
 
 }
