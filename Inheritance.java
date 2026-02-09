1c7bbb0bb76b5a4a76557a81cae66996157b9a1e
varshadevi18 <itzvarshadevi@gmail.com>
2026-02-09 20:07:29 +0530

    Inheritance in java

---
+++ Inheritance.java
@@ -0,0 +1,32 @@
+class Calc{
+
+    public int add(int n1,int n2){
+        return n1+n2;
+    }
+    public int sub(int n1,int n2){
+        return n1-n2;
+    }
+}
+class Advcalc extends Calc{
+    public int power(int a,int b){
+        return (int) Math.pow(a,b);
+    }
+    // child class
+    //with this object we can access method from this and parent class
+}
+public class Inheritance {
+    public static void main(String[] args){
+        // Calc obj= new Calc();
+        // int sum=obj.add(5,6);
+        // int sub=obj.sub(5, 3);
+        Advcalc obj1= new Advcalc();
+        int sum= obj1.add(5,6);
+        int sub=obj1.sub(6,3);
+        int p= obj1.power(4, 2);
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(p);
+
+    }  
+
+}
